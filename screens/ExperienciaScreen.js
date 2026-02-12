8a615010fa9cf3222aa3c1e8219822db2a5402f0
BUDAV-25 <bennydelamparo@gmail.com>
2026-02-11 20:35:57 -0400

    Video

--- screens/ExperienciaScreen.js
+++ screens/ExperienciaScreen.js
@@ -3,7 +3,7 @@ import { WebView } from 'react-native-webview';
 export default function ExperienciaScreen() {
   return (
     <WebView
-      source={{ uri: 'https://www.youtube.com/embed/TU_VIDEO_ID' }}
+      source={{ uri: 'https://youtu.be/oJiTlDOSKls' }}
       style={{ flex: 1, backgroundColor: '#000' }}
     />
   );
